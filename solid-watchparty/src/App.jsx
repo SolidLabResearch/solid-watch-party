a7b9543e584ed6193bcba57e870d15c45e145089
eliasnijs <elias.nijs@gmail.com>
2023-11-15 10:07:57 +0100

    :art: repo setup

--- solid-watchparty/src/App.jsx
+++ solid-watchparty/src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LoginPage from './pages/LoginPage'
 
 function App() {
-  return (
-    <>
-      <div>
-      	<h1 class="sw-fs-1">solid-watchparty</h1>
-      </div>
-    </>
-  )
+	return (
+		<LoginPage/>
+	);
 }
 
-export default App
+export default App;
---
+++ solid-watchparty/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+function LoginPage()
+{
+	return (
+		<>
+      <div>
+      	<h1 class="sw-fs-1">solid-watchparty</h1>
+      </div>
+    </>
+  )
+}
+
+export default LoginPage;
