df744052df758f4a14fc7bbbed974e89e911d684
eliasnijs <elias.nijs@gmail.com>
2024-02-28 14:48:49 +0100

    :ambulance: add basepath to react router

--- solid-watchparty/src/config.js
+++ solid-watchparty/src/config.js
@@ -1,5 +1,5 @@
+export const BASEPATH = '/solid-watch-party'
 
 /* NOTE(Elias): default POD directory structure related to the watchparty */
 export const ROOMS_ROOT = 'watchparties/myRooms';
 export const MESSAGES_ROOT = 'watchparties/myMessages';
-
